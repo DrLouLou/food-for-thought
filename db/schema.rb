fce5d32b97d9d62bd303e7ad189d713994003922
Louis Choo-Choy <lchoochoy@gmail.com>
2020-02-24 17:00:30 +0000

    fixed models bug

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_154301) do
+ActiveRecord::Schema.define(version: 2020_02_24_165918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "dinners", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.bigint "user_id", null: false
+    t.string "location"
+    t.string "cuisine"
+    t.integer "capacity"
+    t.integer "price"
+    t.string "title"
+    t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_dinners_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_dinners_on_reset_password_token", unique: true
+    t.index ["user_id"], name: "index_dinners_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +40,5 @@ ActiveRecord::Schema.define(version: 2020_02_24_154301) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "dinners", "users"
 end
