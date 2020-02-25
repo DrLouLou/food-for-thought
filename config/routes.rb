b8d272d4a1d7a2c4372c51a74495af11bfa22621
Otto <ottoburney@gmail.com>
2020-02-25 11:06:30 +0000

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-
   root to: 'pages#home'
+
+  resources :dinners do
+    resources :bookings, only: [:index, :new, :create]
+  end
+
+  resources :bookings, except: [:index, :new, :create] do
+    resources :reviews, only: [:index, :new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
