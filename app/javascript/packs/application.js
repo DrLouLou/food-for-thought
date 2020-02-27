77c681344fb1cd50ba2d9cf40863993126128b67
Niccolas Farani <farani.niccolas96@gmail.com>
2020-02-27 15:48:28 +0000

    made further changes to signup and login pages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,3 +23,14 @@ require("channels")
 // ----------------------------------------------------
 
 import "bootstrap";
+import Typed from 'typed.js'
+
+var options = {
+  strings: ['Sign-up', 'What are you waiting for?', 'Yo'],
+  typeSpeed: 20,
+  backSpeed: 20,
+  loop: true,
+  shuffle: true
+};
+
+var typed = new Typed('.sick-js', options)
