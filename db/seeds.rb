633661ed3143cad43dc2fc83b4cb20533dbdbe3b
Carl Diederichs <carl-diederichs@t-online.de>
2020-02-28 13:03:40 +0000

    Improved Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,26 +16,29 @@ guest_user_log_in = User.create(email: 'zach@gmail.com', password: '111111')
 host_user_log_in = User.create(email:'tom@gmail.com', password:'111111')
 cuisine = ['Italian', 'Spanish', 'Mexican', 'Chinese', 'Indian']
 dishes = {
-  'Italian' => %w[Pizza Pasta Cotoletta Bistecca Ragu Cappuccino],
+  'Italian' => %w[Pizza Pasta Bruschetta],
   'Spanish' => %w[Tapas Morcilias Paella Alhambra Patanegra Polpo],
   'Mexican' => %w[Tacos Burritos Salad Guacamole Nachos Margarita],
-  'Chinese' => %w[Ramen Friedrice Wok Bun Dimsum Xialongbao],
+  'Chinese' => %w[Ramen Friedrice Wok Bun Dimsum Dumpling],
   'Indian' => %w[Curry Masala Naan Lassie Rice]
 }
-100.times do |dinner|
+500.times do |dinner|
   current_cuisine = cuisine.sample
-  url = "https://source.unsplash.com/1600x900?#{current_cuisine},Food"
+  dish = dishes[current_cuisine].sample
+  url = "https://source.unsplash.com/1600x900?Food,#{current_cuisine},#{dish}"
   dinners = Dinner.create!(
     user: guest_user_log_in && host_user_log_in,
     location: 'London',
     cuisine: current_cuisine,
     capacity: 1,
     price: rand(2..5),
-    title: dishes[current_cuisine].sample,
+    title: dish,
     datetime: Date.today,
     image_url: url
   )
 end
 
 puts 'Finished!'
+
+
 
